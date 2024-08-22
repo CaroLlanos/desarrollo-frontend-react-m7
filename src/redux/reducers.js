bce1ae62c6fcaec08133c5b880a4c0e1f441fbf1
CaroLlanos <andreallr1999@gmail.com>
2024-08-22 19:12:08 -0400

    init redux 3 y loginform

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import defaultReducer from './default/defaultReducers';
 import productReducer from './Product/productReducer';
+import formReducer from './Form/formReducer';
 
 const rootReducer = combineReducers({
     default: defaultReducer,
-    products: productReducer
+    products: productReducer,
+    form: formReducer
 });
 
 export default rootReducer;
--- src/screens/Forms/loginForm.jsx
+++ src/screens/Forms/loginForm.jsx
@@ -1,7 +1,41 @@
+import useForm from "../../components/hooks/useForm";
+import { useSelector, useDispatch } from 'react-redux';
+import { saveFormData } from "../../redux/Form/formActions";
+
 const LoginForm = () => {
+    const [values, handleChange] = useForm({ username: '', email: '', password: ''});
+    const form = useSelector(state => state.form);
+    const dispatch = useDispatch();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(values);
+        dispatch(saveFormData(values));
+    }
+
     return (
-        <div>
-            <h1>Componenete LoginForm</h1>
+        <div className = "container">
+            <form onSubmit={handleSubmit}>
+                <h5>username: {form.formData.username}</h5>
+                <h5>email: {form.formData.email}</h5>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" value={values.username} onChange={handleChange}>
+                    </input>
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" value={values.email} onChange={handleChange}>
+                    </input>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" value={values.password} onChange={handleChange}>
+                    </input>
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
